6ddadd59674f519ed0e3b508f8527a8c6b80cb2e
Ajith Hussain <csy0013@googlemail.com>
2017-04-10 21:37:10 +1000

    Added css classes

--- layout.go
+++ layout.go
@@ -1,10 +1,22 @@
 package bootstrap4
 
 import (
-	"github.com/sparkymat/webdsl/css"
+	"github.com/sparkymat/bootstrap4/classes"
 	"github.com/sparkymat/webdsl/html"
 )
 
 func Container(children ...html.ChildNode) *html.Node {
-	return html.Div().Class(css.Class("container")).Add(children...)
+	return html.Div().Class(classes.Container).Add(children...)
+}
+
+func ContainerFluid(children ...html.ChildNode) *html.Node {
+	return html.Div().Class(classes.ContainerFluid).Add(children...)
+}
+
+func Row(children ...html.ChildNode) *html.Node {
+	return html.Div().Class(classes.Row).Add(children...)
+}
+
+func Col(children ...html.ChildNode) *html.Node {
+	return html.Div().Class(classes.Col).Add(children...)
 }
